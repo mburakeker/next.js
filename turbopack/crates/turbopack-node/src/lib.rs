afe78e89bb02c72fa2d029bb045f19cf817b8baa
강동윤 (Donny) <kdy1997.dev@gmail.com>
2025-05-02 14:45:13 -0700

    turbopack-node

--- turbopack/crates/turbopack-node/src/lib.rs
+++ turbopack/crates/turbopack-node/src/lib.rs
@@ -10,7 +10,7 @@ use rustc_hash::FxHashMap;
 use turbo_rcstr::RcStr;
 use turbo_tasks::{
     graph::{AdjacencyMap, GraphTraversal},
-    FxIndexSet, ResolvedVc, TryJoinIterExt, ValueToString, Vc,
+    FxIndexSet, ResolvedVc, TryJoinIterExt, Vc,
 };
 use turbo_tasks_env::ProcessEnv;
 use turbo_tasks_fs::{to_sys_path, File, FileSystemPath};
@@ -44,7 +44,11 @@ async fn emit(
     intermediate_output_path: FileSystemPath,
 ) -> Result<()> {
     for asset in internal_assets(intermediate_asset, intermediate_output_path).await? {
-        let _ = asset.content().write(asset.path()).resolve().await?;
+        let _ = asset
+            .content()
+            .write((*asset.path().await?).clone())
+            .resolve()
+            .await?;
     }
     Ok(())
 }
@@ -84,8 +88,8 @@ async fn internal_assets_for_source_mapping(
     intermediate_asset: Vc<Box<dyn OutputAsset>>,
     intermediate_output_path: FileSystemPath,
 ) -> Result<Vc<AssetsForSourceMapping>> {
-    let internal_assets = internal_assets(intermediate_asset, intermediate_output_path).await?;
-    let intermediate_output_path = &*intermediate_output_path.await?;
+    let internal_assets =
+        internal_assets(intermediate_asset, intermediate_output_path.clone()).await?;
     let mut internal_assets_for_source_mapping = FxHashMap::default();
     for asset in internal_assets.iter() {
         if let Some(generate_source_map) =
@@ -126,33 +130,35 @@ async fn separate_assets_operation(
     intermediate_asset: ResolvedVc<Box<dyn OutputAsset>>,
     intermediate_output_path: FileSystemPath,
 ) -> Result<Vc<SeparatedAssets>> {
-    let intermediate_output_path = &*intermediate_output_path.await?;
     #[derive(PartialEq, Eq, Hash, Clone, Copy)]
     enum Type {
         Internal(ResolvedVc<Box<dyn OutputAsset>>),
         External(ResolvedVc<Box<dyn OutputAsset>>),
     }
-    let get_asset_children = |asset| async move {
-        let Type::Internal(asset) = asset else {
-            return Ok(Vec::new());
-        };
-        asset
-            .references()
-            .await?
-            .iter()
-            .map(|asset| async {
-                // Assets within the output directory are considered as "internal" and all
-                // others as "external". We follow references on "internal" assets, but do not
-                // look into references of "external" assets, since there are no "internal"
-                // assets behind "externals"
-                if asset.path().await?.is_inside_ref(intermediate_output_path) {
-                    Ok(Type::Internal(*asset))
-                } else {
-                    Ok(Type::External(*asset))
-                }
-            })
-            .try_join()
-            .await
+    let get_asset_children = |asset| {
+        let intermediate_output_path = intermediate_output_path.clone();
+        async move {
+            let Type::Internal(asset) = asset else {
+                return Ok(Vec::new());
+            };
+            asset
+                .references()
+                .await?
+                .iter()
+                .map(|asset| async {
+                    // Assets within the output directory are considered as "internal" and all
+                    // others as "external". We follow references on "internal" assets, but do not
+                    // look into references of "external" assets, since there are no "internal"
+                    // assets behind "externals"
+                    if asset.path().await?.is_inside_ref(&intermediate_output_path) {
+                        Ok(Type::Internal(*asset))
+                    } else {
+                        Ok(Type::External(*asset))
+                    }
+                })
+                .try_join()
+                .await
+        }
     };
 
     let graph = AdjacencyMap::new()
@@ -186,14 +192,14 @@ async fn separate_assets_operation(
 /// Emit a basic package.json that sets the type of the package to commonjs.
 /// Currently code generated for Node is CommonJS, while authored code may be
 /// ESM, for example.
-fn emit_package_json(dir: FileSystemPath) -> Vc<()> {
-    emit(
+fn emit_package_json(dir: FileSystemPath) -> Result<Vc<()>> {
+    Ok(emit(
         Vc::upcast(VirtualOutputAsset::new(
-            dir.join("package.json".into()),
+            dir.join("package.json".into())?,
             AssetContent::file(File::from("{\"type\": \"commonjs\"}").into()),
         )),
         dir,
-    )
+    ))
 }
 
 /// Creates a node.js renderer pool for an entrypoint.
@@ -207,24 +213,26 @@ pub async fn get_renderer_pool_operation(
     project_dir: FileSystemPath,
     debug: bool,
 ) -> Result<Vc<NodeJsPool>> {
-    emit_package_json(*intermediate_output_path).await?;
+    emit_package_json(intermediate_output_path.clone())?.await?;
 
-    let _ = emit(*intermediate_asset, *output_root).resolve().await?;
+    let _ = emit(*intermediate_asset, output_root.clone())
+        .resolve()
+        .await?;
     let assets_for_source_mapping =
-        internal_assets_for_source_mapping(*intermediate_asset, *output_root);
+        internal_assets_for_source_mapping(*intermediate_asset, output_root.clone());
 
-    let entrypoint = intermediate_asset.path();
+    let entrypoint = (*intermediate_asset.path().await?).clone();
 
-    let Some(cwd) = to_sys_path(*cwd).await? else {
+    let Some(cwd) = to_sys_path(cwd.clone()).await? else {
         bail!(
             "can only render from a disk filesystem, but `cwd = {}`",
-            cwd.to_string().await?
+            cwd.to_string()
         );
     };
-    let Some(entrypoint) = to_sys_path(entrypoint).await? else {
+    let Some(entrypoint) = to_sys_path(entrypoint.clone()).await? else {
         bail!(
             "can only render from a disk filesystem, but `entrypoint = {}`",
-            entrypoint.to_string().await?
+            entrypoint.to_string()
         );
     };
     // Invalidate pool when code content changes
@@ -254,23 +262,28 @@ pub async fn get_intermediate_asset(
     main_entry: ResolvedVc<Box<dyn EvaluatableAsset>>,
     other_entries: Vc<EvaluatableAssets>,
 ) -> Result<Vc<Box<dyn OutputAsset>>> {
-    Ok(Vc::upcast(chunking_context.root_entry_chunk_group_asset(
-        chunking_context.chunk_path(None, main_entry.ident(), ".js".into()),
-        other_entries.with_entry(*main_entry),
-        ModuleGraph::from_modules(
-            Vc::cell(vec![ChunkGroupEntry::Entry(
-                other_entries
-                    .await?
-                    .into_iter()
-                    .copied()
-                    .chain(std::iter::once(main_entry))
-                    .map(ResolvedVc::upcast)
-                    .collect(),
-            )]),
-            false,
+    Ok(Vc::upcast(
+        chunking_context.root_entry_chunk_group_asset(
+            (*chunking_context
+                .chunk_path(None, main_entry.ident(), ".js".into())
+                .await?)
+                .clone(),
+            other_entries.with_entry(*main_entry),
+            ModuleGraph::from_modules(
+                Vc::cell(vec![ChunkGroupEntry::Entry(
+                    other_entries
+                        .await?
+                        .into_iter()
+                        .copied()
+                        .chain(std::iter::once(main_entry))
+                        .map(ResolvedVc::upcast)
+                        .collect(),
+                )]),
+                false,
+            ),
+            OutputAssets::empty(),
         ),
-        OutputAssets::empty(),
-    )))
+    ))
 }
 
 #[derive(Clone, Debug)]
