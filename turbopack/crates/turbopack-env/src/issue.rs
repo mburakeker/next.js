0afec157df43210f9c9536318bf2bea9f3353b1f
강동윤 (Donny) <kdy1997.dev@gmail.com>
2025-05-02 14:16:28 -0700

    turbopack-env

--- turbopack/crates/turbopack-env/src/issue.rs
+++ turbopack/crates/turbopack-env/src/issue.rs
@@ -23,7 +23,7 @@ impl Issue for ProcessEnvIssue {
 
     #[turbo_tasks::function]
     fn file_path(&self) -> Vc<FileSystemPath> {
-        self.path.cell()
+        self.path.clone().cell()
     }
 
     #[turbo_tasks::function]
